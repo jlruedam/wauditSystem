44d7716e0e3eb08f55308183b3c8ff28aa342e99
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-21 09:51:30 -0500

    setting

--- wauditSystem/settings.py
+++ wauditSystem/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'universe'
+    'universe.apps.UniverseConfig',
 ]
 
 MIDDLEWARE = [
