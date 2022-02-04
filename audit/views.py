ff7c60ffac04aebb1bcfd1e6d256a8a41a5d00f6
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-02-04 14:01:40 -0500

    crear submodulo de crear auditoria del Modulo Auditorias

--- audit/views.py
+++ audit/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "./audit/index.html")
 
+def subModuleCreateAudit(request):
+    return render(request, "./audit/subModuleCreateAudit.html")
