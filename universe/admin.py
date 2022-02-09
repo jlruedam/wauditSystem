670e44c4d78aaf411f3871651dbc0d479c084c3e
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-02-09 06:32:05 -0500

    Probar import_export para agregar al admin y descargar bases de datos en excell

--- universe/admin.py
+++ universe/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-admin.site.register(Persona)
+
+class PersonaResource(resources.ModelResource):
+    class Meta:
+        model=Persona
+
+class PersonaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields=['id_user']
+    list_display=('id_user','name','lastname')
+    resource_class=PersonaResource
+    
+
+admin.site.register(Persona, PersonaAdmin)
 admin.site.register(UniverseManageResponsable)
 admin.site.register(UniverseMacroproces)
 admin.site.register(UniverseProces)
