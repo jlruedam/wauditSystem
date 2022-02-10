769499308c479cb5f2e746d2f3a9c95819629914
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-02-10 08:22:45 -0500

    cambios en el admin para agregar importExport

--- universe/admin.py
+++ universe/admin.py
@@ -12,9 +12,17 @@ class PersonaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields=['id_user']
     list_display=('id_user','name','lastname')
     resource_class=PersonaResource
+
+class UniverseManageResponsableAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields=['codeManage']
+    list_display=('codeManage','responsable')
+
+class UniverseManageResponsableResource(resources.ModelResource):
+    class Meta:
+        model=UniverseManageResponsable
     
 
 admin.site.register(Persona, PersonaAdmin)
-admin.site.register(UniverseManageResponsable)
+admin.site.register(UniverseManageResponsable, UniverseManageResponsableAdmin)
 admin.site.register(UniverseMacroproces)
 admin.site.register(UniverseProces)
